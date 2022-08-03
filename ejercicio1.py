917650c73acd578cebd10635c5975db618b51964
vickivi <108895967+vickivi@users.noreply.github.com>
2022-08-03 11:11:44 -0300

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+print ("ingrese la velocidad y el tiempo de la unidad movil")
+
+V = float( input("velocidad: "))
+
+T = int (input("tiempo: "))
+
+D= V * T 
+
+print ( " LA DISTANCIA RECORRIDA POR LA UNIDAD MOVIL " ) (D)
+
+
+
+ 
