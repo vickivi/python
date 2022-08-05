335cb22b7f0e8c08efea1125d9d691aed466cb20
vickivi <108895967+vickivi@users.noreply.github.com>
2022-08-05 17:45:28 -0300

    ki

---
+++ ejercicio4.py
@@ -0,0 +1,16 @@
+print ("ingrese cantidad de partidos ganados")
+pg = int (input ("pg: "))
+
+print ("ingrese cantidad de partidos empatados")
+pe = int (input ("pe: "))
+
+print ("ingrese cantidad de partidos perdidos")
+pp = int (input ("pp; "))
+
+ppg = pg * 3
+ppe = pe *1
+ppp = pp * 0
+
+pt = ppg + ppe + ppp 
+
+print (" el puntaje total es dev" ,pt)
